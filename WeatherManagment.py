49f313635491ec20da0f92f5511c15664233932a
V3xe <jarzabek.aleksander@icloud.com>
2024-06-12 15:28:00 +0200

    Started getting weather data per city

--- WeatherManagment.py
+++ WeatherManagment.py
@@ -1,7 +1,6 @@
 import requests
 import json
 from abc import ABC, abstractmethod
-from urllib.error import HTTPError
 
 
 class WeatherManageTemplate(ABC):
@@ -14,20 +13,12 @@ class WeatherManageTemplate(ABC):
         ...
 
     @abstractmethod
-    def GetCityListy()-> list:
-        ...
-
-    @abstractmethod
-    def CheckApiStatus() -> None:
-        ...
-
-    @abstractmethod
     def GetWeatherPerCity() -> str:
         ...
 
 
 class WeatherMange(WeatherManageTemplate):
-
+    ApiCall: str = 'https://api.openweathermap.org/data/2.5/weather?q={CityName}&appid={ApiKey}'
     @staticmethod
     #reading data from JSON file
     def GetDataFromJson() -> dict:
@@ -46,34 +37,42 @@ class WeatherMange(WeatherManageTemplate):
         try:
             while boolFlag:
                 counter += 1
-                r = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={ApiKey}')
-                print(r.status_code)
-
+                r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=London&appid={ApiKey}')
                 if r.status_code == 200:
-                    print('Connection established...')
+                    print(f'Connection established, status {r.status_code}...')
                     boolFlag = False
                 elif counter < 5:
-                    print(f'Could not establish connection, try -> {str(counter)}')
+                    print(f'Could not establish connection, error {r.status_code}, try -> {str(counter)}')
                 elif counter == 5:
-                    boolFlag = False
                     raise requests.exceptions.HTTPError
 
         except requests.exceptions.HTTPError as e:
-            print(f'Cannot establish connection to server error...')
+            print(f'Cannot establish connection to server error, {e}...')
+
+    #@staticmethod
+    #get city list
+    #def GetCityListy(cityList: list) -> list:
+    #    print(cityList)
+    #    return cityList
 
     @staticmethod
-    def GetCityListy(ApiKey:str) -> list:
-        print(ApiKey)
+    # Think of async request, send all requests and gather responses later?
+    def GetWeatherPerCity(ApiCall: str, ApiKey: str, cityList: list) -> dict:
+        for city in cityList:
+            call = ApiCall.replace('{CityName}',city).replace('{ApiKey}',ApiKey)
+            r = requests.get(call)
+            print(r.json())
+            print(call)
+            print(f'{ApiCall}, {ApiKey}, {cityList}')
 
-    def CheckApiStatus() -> None:
-        ...
-
-    def GetWeatherPerCity() -> str:
-        ...
 
 
-c = WeatherMange()
-a = c.GetDataFromJson()
-b = c.CheckApi(a['apiKey'])
+WeatherObject = WeatherMange()
+ConfigJSON: dict = WeatherObject.GetDataFromJson()
+print(ConfigJSON)
+WeatherObject.CheckApi(ConfigJSON['apiKey'])
+WeatherObject.GetWeatherPerCity(WeatherMange.ApiCall,ConfigJSON['apiKey'],ConfigJSON['cityList'])
+
+
 #c.GetCityListy(a)
 
