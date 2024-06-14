f4083fad28ced98cb8924e6e0de73adf060772e9
V3xe <ziarenka_odmowa_0i@icloud.com>
2024-06-14 15:40:21 +0200

    - Getting data from response

--- WeatherManagment.py
+++ WeatherManagment.py
@@ -48,22 +48,38 @@ class WeatherMange(WeatherManageTemplate):
 
         except requests.exceptions.HTTPError as e:
             print(f'Cannot establish connection to server error, {e}...')
-
-    #@staticmethod
-    #get city list
-    #def GetCityListy(cityList: list) -> list:
-    #    print(cityList)
-    #    return cityList
+    @staticmethod
+    def getCityDataFromResponse(keyFromList: list,cityWeather: dict,newDictKey: str,oldDictKey:str) -> dict:
+        KeyDict: dict = keyFromList[0]
+        cityWeather[newDictKey] = KeyDict[oldDictKey]
+        print(KeyDict)
+        return cityWeather
 
     @staticmethod
     # Think of async request, send all requests and gather responses later?
     def GetWeatherPerCity(ApiCall: str, ApiKey: str, cityList: list) -> dict:
+        cityWeather: dict = {}
         for city in cityList:
             call = ApiCall.replace('{CityName}',city).replace('{ApiKey}',ApiKey)
+            cityWeather['cityName'] = city
             r = requests.get(call)
-            print(r.json())
-            print(call)
-            print(f'{ApiCall}, {ApiKey}, {cityList}')
+            data: dict = r.json()
+            print(data)
+            WeatherMange.getCityDataFromResponse(data.get('weather'),cityWeather,'weather','main')
+            print(cityWeather)
+
+
+            #General weather description
+            #MainKey: list = data.get('weather')
+            #MainDict: dict = MainKey[0]
+            #print(MainDict['main'])
+            #cityWeather['weather'] = MainDict['main']
+            #City temperature
+            #Temperature: list = data.get('main')
+            #print(cityWeather)
+            #print(call)
+            #print(f'{ApiCall}, {ApiKey}, {cityList}')
+
 
 
 
@@ -72,7 +88,4 @@ ConfigJSON: dict = WeatherObject.GetDataFromJson()
 print(ConfigJSON)
 WeatherObject.CheckApi(ConfigJSON['apiKey'])
 WeatherObject.GetWeatherPerCity(WeatherMange.ApiCall,ConfigJSON['apiKey'],ConfigJSON['cityList'])
-
-
-#c.GetCityListy(a)
 
